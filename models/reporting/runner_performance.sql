7c6b03533d273a22e6f22a385e1ef68d62ce0acc
Grace Seda <grace.seda@fishtownanalytics.com>
2025-05-21 14:26:16 +0000

    ref snapshots

--- models/reporting/runner_performance.sql
+++ models/reporting/runner_performance.sql
@@ -5,5 +5,5 @@ select
     pb.event,
     pb.time_minutes as personal_best_time
 from {{ ref('runner_profile_snap')}} prof
-left join {{('runners_current_pb') }} pb
+left join {{ref('runners_current_pb') }} pb
     on prof.runner_id = pb.runner_id
