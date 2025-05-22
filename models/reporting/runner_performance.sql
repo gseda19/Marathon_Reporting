a0952606885c4a71d55a05b34bb8bd38ed04d49c
Grace Seda <grace.seda@fishtownanalytics.com>
2025-05-22 01:15:10 +0000

    remove models

--- models/reporting/runner_performance.sql
+++
@@ -1,9 +0,0 @@
-select
-    prof.runner_id,
-    prof.name,
-    prof.gender,
-    pb.event,
-    pb.time_minutes as personal_best_time
-from {{ ref('runner_profile_snap')}} prof
-left join {{ref('runners_current_pb') }} pb
-    on prof.runner_id = pb.runner_id
